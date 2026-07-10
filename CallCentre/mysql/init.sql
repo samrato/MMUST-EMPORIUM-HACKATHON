047c272cf050a10d5e34135ec5444d914a586704
samrato <willingtonejumamconyango@gmail.com>
2026-07-10 23:33:57 +0300

    feat:remove the non independednt backend :

--- CallCentre/mysql/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS asterisk;
-GRANT ALL PRIVILEGES ON `asterisk`.* TO 'freepbxuser'@'%';
-
-CREATE DATABASE IF NOT EXISTS asteriskcdrdb;
-GRANT ALL PRIVILEGES ON `asteriskcdrdb`.* TO 'freepbxuser'@'%';
-
-FLUSH PRIVILEGES;
